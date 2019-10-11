7c8e170c87310fbc997471a12a09291dbfd7ffe2
Wesley Robinson <wrobinson@luno.com>
2019-10-11 11:03:17 +0200

    Player_rounds table with lookup and insert

--- player/db/schema.sql
+++ player/db/schema.sql
@@ -33,4 +33,19 @@ create table round_parts (
 
     primary key (id),
     unique by_match_round_player(`match_id`, `round_id`, `player_id`)
+
+create table round_parts (
+    id bigint not null auto_increment,
+
+    match_id int not null,
+    round_id int not null,
+    player_id varchar(255) not null,
+    rank int not null,
+    p1_part int not null,
+    p2_part int not null,
+    p3_part int not null,
+    p4_part int not null,
+
+    primary key (id),
+    unique by_match_round_player(match_id, round_id, player_id)
 );
