c647f6cff87a2027eb639d243c3582f5a31e7d06
Wesley Robinson <wrobinson@luno.com>
2019-10-11 14:26:04 +0200

    Add team name const to ops

--- player/ops/ops.go
+++ player/ops/ops.go
@@ -2,6 +2,8 @@ package ops
 
 import "github.com/adamhicks/ctrlaltdefeat/player/config"
 
+const TeamName = "CtrlAltDefeat"
+
 func RunLoops(c config.Config, backends Backends) {
 	// Add loops to run here!
 	// e.g. go JoinRoundsForever()
