bb541a67bb2dbcf26963ca5c2b36dcda7ad4d30e
Wesley Robinson <wrobinson@luno.com>
2019-10-11 13:25:46 +0200

    Fix conflict thingy v2.0

--- player/types.go
+++ player/types.go
@@ -45,15 +45,3 @@ var (
 	PlayerRoundStatusRoundEnded      PlayerRoundStatus = 8
 	PlayerRoundStatusSentinel        PlayerRoundStatus = 9
 )
-
-type RoundParts struct {
-	ID       int64
-	MatchID  int64
-	RoundID  int64
-	PlayerID string
-	Rank     int64
-	P1Part   int64
-	P2Part   int64
-	P3Part   int64
-	P4Part   int64
-}
