8490a67fb69f25d84452388d937c1364d60b9189
Wesley Robinson <wrobinson@luno.com>
2019-10-11 11:03:17 +0200

    Player_rounds table with lookup and insert

--- player/types.go
+++ player/types.go
@@ -9,3 +9,15 @@ type PartInfo struct {
 	Player string
 	Part   int64
 }
+
+type RoundParts struct {
+	ID       int64
+	MatchID  int64
+	RoundID  int64
+	PlayerID string
+	Rank     int64
+	P1Part   int64
+	P2Part   int64
+	P3Part   int64
+	P4Part   int64
+}
