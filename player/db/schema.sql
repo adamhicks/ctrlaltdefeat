01eb7a8a1054166a52a09d4f847d576ace955fb2
Wesley Robinson <wrobinson@luno.com>
2019-10-11 11:03:17 +0200

    Player_rounds table with lookup and insert

--- player/db/schema.sql
+++ player/db/schema.sql
@@ -18,4 +18,19 @@ create table player_rounds_events (
   timestamp datetime not null,
   foreign_id bigint not null,
   type int not null
+
+create table round_parts (
+    `id` bigint not null auto_increment,
+
+    `match_id` int not null,
+    `round_id` int not null,
+    `player_id` varchar(255) not null,
+    `rank` int not null,
+    `p1_part` int not null,
+    `p2_part` int not null,
+    `p3_part` int not null,
+    `p4_part` int not null,
+
+    primary key (id),
+    unique by_match_round_player(`match_id`, `round_id`, `player_id`)
 );
--- player/types.go
+++ player/types.go
@@ -45,3 +45,15 @@ var (
 	PlayerRoundStatusRoundEnded      PlayerRoundStatus = 8
 	PlayerRoundStatusSentinel        PlayerRoundStatus = 9
 )
+
+type RoundParts struct {
+	ID       int64
+	MatchID  int64
+	RoundID  int64
+	PlayerID string
+	Rank     int64
+	P1Part   int64
+	P2Part   int64
+	P3Part   int64
+	P4Part   int64
+}
