8df06f2a9edccd620cdbff196fc6a7da5ee95156
wesrobin-luno <54845490+wesrobin-luno@users.noreply.github.com>
2019-10-11 16:23:30 +0200

    Move to players to round ended (#18)
    * Move to players to round ended
    
    * Review comments

--- player/ops/f_ending.go
+++ player/ops/f_ending.go
@@ -1,4 +1,57 @@
 package ops
 
-//ConsumeRoundEndedForever
+import (
+	"context"
+	"github.com/adamhicks/ctrlaltdefeat/player"
+	"github.com/adamhicks/ctrlaltdefeat/player/config"
+	"github.com/adamhicks/ctrlaltdefeat/player/db/cursors"
+	"github.com/adamhicks/ctrlaltdefeat/player/db/rounds"
+	"github.com/corverroos/unsure"
+	"github.com/corverroos/unsure/engine"
+	"github.com/luno/fate"
+	"github.com/luno/reflex"
+)
+
+const endingCursor = "ending_events"
+
 //Listen for EventTypeRoundSuccess and EventTypeRoundFailed, transition PR to RoundEnded
+func ConsumeRoundEndedForever(c config.Config, b Backends) {
+	processRoundEndedEvents := func(ctx context.Context, fate fate.Fate, event *reflex.Event) error {
+		if !reflex.IsType(event.Type, engine.EventTypeRoundFailed) && !reflex.IsType(event.Type, engine.EventTypeRoundSuccess) {
+			return fate.Tempt()
+		}
+
+		round, err := rounds.LookupRound(ctx, b.DB(), int(event.ForeignIDInt()))
+		if err != nil {
+			return err
+		}
+
+		tx, err := b.DB().Begin()
+		if err != nil {
+			return err
+		}
+		defer tx.Rollback()
+
+		if int(round.Status) == player.PlayerRoundStatusRoundExcluded.Enum() {
+			notify, err := rounds.EndedExcluded(ctx, tx, round.ID)
+			if err != nil {
+				return err
+			}
+			defer notify()
+			return tx.Commit()
+		} else if int(round.Status) == player.PlayerRoundStatusRoundSubmitted.Enum() {
+			notify, err := rounds.EndedJoined(ctx, tx, round.ID)
+			if err != nil {
+				return err
+			}
+			defer notify()
+			return tx.Commit()
+		}
+
+		return fate.Tempt()
+	}
+
+	consumer := reflex.NewConsumer(endingCursor, processRoundEndedEvents)
+	consumable := reflex.NewConsumable(b.EngineClient().Stream, cursors.ToStore(b.DB()))
+	unsure.ConsumeForever(unsure.FatedContext, consumable.Consume, consumer)
+}
