41300c655504febaff466eec8382dba24a99b885
Adam Hicks <ahicks@luno.com>
2019-10-11 13:00:48 +0100

    db: Lookup rounds in state

---
+++ player/db/rounds/lookup.go
@@ -0,0 +1,26 @@
+package rounds
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/adamhicks/ctrlaltdefeat/player"
+)
+
+func ListWithStatus(ctx context.Context, dbc sql.DB, s player.PlayerRoundStatus) ([]player.PlayerRound, error) {
+	rows, err := dbc.QueryContext(ctx, selectPrefix+"status=?", s.Enum())
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var rounds []player.PlayerRound
+	for rows.Next() {
+		r, err := scan(rows)
+		if err != nil {
+			return nil, err
+		}
+		rounds = append(rounds, *r)
+	}
+	return rounds, nil
+}
